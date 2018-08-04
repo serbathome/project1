50d8af33b213532058eb3720d8735fa97c191f88
Sergey Birukov <serb@microsoft.com>
2018-08-04 19:42:44 +0300

    removed env_vars

--- env_vars.bat
+++
@@ -1,4 +0,0 @@
-echo off
-set FLASK_APP=application.py
-set FLASK_DEBUG=1
-set DATABASE_URL=postgres://mhkctvzhdizwzr:aec8ae5cb9986a0ac21406e83eea64056e5e9d4229814daaeb2796caef9a8ef1@ec2-54-228-251-254.eu-west-1.compute.amazonaws.com:5432/d2fti0ur64qhf9
